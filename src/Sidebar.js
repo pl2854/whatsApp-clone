1a9aa9f81706ca33b389ea227486f71d9bf4e220
pl2854 <pl2854@d.turufi>
2021-03-01 10:42:07 +0200

    commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+
 import "./Sidebar.css";
 
 function Sidebar() {
@@ -7,15 +12,18 @@ function Sidebar() {
         <div className="sidebar">
             <div className="sidebar__header">
                 <Avatar />
+                <div className="sidebar__headerRight">
+                <DonutLargeIcon />
+                <ChatIcon />
+                <MoreVertIcon />
+
+                </div>
+
             </div>
 
-            <div className="sidebar__search">
+            <div className="sidebar__search"></div>
 
-            </div>
-
-            <div className="sidebar__chats">
-
-            </div>
+            <div className="sidebar__chats"></div>
         
         </div>
     );
