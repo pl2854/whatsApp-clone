977cbd789eedf787024a738d06959e805bfc3a7c
pl2854 <pl2854@d.turufi>
2021-03-09 13:43:43 +0200

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDlb_3ztdzP3i5jpKDMgrxkUzTqRSW1vA4",
     authDomain: "whatsapp-clone-cd5a1.firebaseapp.com",
@@ -8,3 +9,11 @@ const firebaseConfig = {
     appId: "1:809242251485:web:66d9a9b7d4695b77ed8b34",
     measurementId: "G-BS8WB991YH"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider};
+  export default db;
